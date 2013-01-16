de0377aaf3b7b28ed7a49c99a9e95a9d67eaa063
Jussi Enroos <McDevon@hoasnet-50dd2e-85.dhcp.inet.fi>
2013-01-16 19:57:27 +0200

    getting data in and out of lua with some tests I found in the web.

--- Mac/Classes/LuaControlManager.m
+++ Mac/Classes/LuaControlManager.m
@@ -22,35 +22,17 @@
     // Create state
     pl_lua_state = luaL_newstate();
     
-    // setup global printing (trace)
-    //lua_pushcclosure (lua_state, printMessage, 0);
-    //lua_setglobal (lua_state, "trace");
-    
-    // Open built-in libraries
-    /*const luaL_Reg* libraries=lua_libraries;
-    for(;libraries->func;libraries++){
-        lua_pushcfunction(pl_lua_state,libraries->func);
-        lua_pushstring(pl_lua_state,libraries->name);
-        lua_call(pl_lua_state,1,0);
-    }*/
 
+
+    // THIS OPENS LUA LIBRARIES TO USE
+    luaL_openlibs(pl_lua_state);
     
+    // THIS SETS MODULE FUNCTIONS AS GLOBALS
     // Open modules
-    /*const luaL_Reg* moduleFunctions=modfuncs;
+    const luaL_Reg* moduleFunctions=modfuncs;
     for(;moduleFunctions->func;moduleFunctions++){
-        lua_pushcfunction(pl_lua_state,modfuncs->func);
-        lua_pushstring(pl_lua_state,modfuncs->name);
-        lua_call(pl_lua_state,1,0);
-    }*/
-    
-    const luaL_Reg *lib = lua_libraries;
-    for(; lib->func != NULL; lib++)
-    {
-        lib->func(pl_lua_state);
-        lua_settop(pl_lua_state, 0);
+        lua_register(pl_lua_state, modfuncs->name, modfuncs->func);
     }
-    
-    //lua_register(pl_lua_state, "pong", modfuncs);
 
     // run the Lua script
     int err = luaL_dofile(pl_lua_state,[fileName cStringUsingEncoding:NSASCIIStringEncoding]);
@@ -59,13 +41,66 @@
         NSLog(@"Lua critical error: %s", lua_tostring(pl_lua_state, -1));
     }
 
-    err = luaL_loadstring(pl_lua_state, "print(\"Hello, world.\")");
+    /*err = luaL_loadstring(pl_lua_state, "print(\"Hello, world.\")");
     if (0 != err) {
-        luaL_error(pl_lua_state, "cannot compile lua file: %s",
-                   lua_tostring(pl_lua_state, -1));
+        NSLog(@"Cannot compile lua file: %s", lua_tostring(pl_lua_state, -1));
+    }*/
+    
+    
+    // Try calling a function
+    lua_getglobal(pl_lua_state, "helloWorld");
+    
+    if (lua_pcall(pl_lua_state, 0, 1, 0) != 0)
+        luaL_error(pl_lua_state, "error calling hello: %s", lua_tostring(pl_lua_state, -1));
+    
+    // Try calling a function with a number parameter
+    lua_getglobal(pl_lua_state, "square");
+    lua_pushnumber(pl_lua_state, 5);
+    
+    if (lua_pcall(pl_lua_state, 1, 1, 0) != 0)
+        luaL_error(pl_lua_state, "error calling square: %s", lua_tostring(pl_lua_state, -1));
+    
+    int mynumber = lua_tonumber(pl_lua_state, -1);
+    NSLog(@"Returned number=%d\n", mynumber);
+    
+    // Try calling a function with two number parameters
+    lua_getglobal(pl_lua_state, "summa");
+    lua_pushnumber(pl_lua_state, 5);
+    lua_pushnumber(pl_lua_state, 82);
+    
+    if (lua_pcall(pl_lua_state, 2, 1, 0) != 0)
+        luaL_error(pl_lua_state, "error calling summa: %s", lua_tostring(pl_lua_state, -1));
+    
+    mynumber = lua_tonumber(pl_lua_state, -1);
+    NSLog(@"Returned number=%d\n", mynumber);
+    
+    // Try calling a function with a table
+    lua_getglobal(pl_lua_state, "tweaktable");             /* Tell it to run callfuncscript.lua->square() */
+    lua_newtable(pl_lua_state);                            /* Push empty table onto stack table now at -1 */
+    lua_pushliteral(pl_lua_state, "fname");                /* Push a key onto the stack, table now at -2 */
+    lua_pushliteral(pl_lua_state, "Margie");               /* Push a value onto the stack, table now at -3 */
+    lua_settable(pl_lua_state, -3);                        /* Take key and value, put into table at -3, */
+    /*  then pop key and value so table again at -1 */
+    
+    lua_pushliteral(pl_lua_state, "lname");                /* Push a key onto the stack, table now at -2 */
+    lua_pushliteral(pl_lua_state, "Martinez");             /* Push a value onto the stack, table now at -3 */
+    lua_settable(pl_lua_state, -3);                        /* Take key and value, put into table at -3, */
+    /*  then pop key and value so table again at -1 */
+    
+    if (lua_pcall(pl_lua_state, 1, 1, 0) != 0)
+        luaL_error(pl_lua_state, "error calling tweaktable: %s", lua_tostring(pl_lua_state, -1));
+    
+    // Get the returned table
+    lua_pushnil(pl_lua_state);  /* Make sure lua_next starts at beginning */
+    const char *k, *v;
+    while (lua_next(pl_lua_state, -2)) {                    /* TABLE LOCATED AT -2 IN STACK */
+        v = lua_tostring(pl_lua_state, -1);                 /* Value at stacktop */
+        lua_pop(pl_lua_state,1);                            /* Remove value */
+        k = lua_tostring(pl_lua_state, -1);                 /* Read key at stacktop, */
+        /* leave in place to guide next lua_next() */
+        printf("Fromc k=>%s<, v=>%s<\n", k, v);
     }
 
-    
     return 1;
 }
 
